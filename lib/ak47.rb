7af4e27f1d8d84bfe6ade5ec13505c057fad3de6
Josh Hull <joshbuddy@gmail.com>
2012-01-04 13:14:26 -0800

    this can be simple, and it should run forever like watch

--- lib/ak47.rb
+++ lib/ak47.rb
@@ -3,32 +3,37 @@ require 'shell_tools'
 require "ak47/version"
 
 module Ak47
+  Reload = Class.new(RuntimeError)
+
   class << self
     def run(*argv)
       command = ShellTools.escape(argv)
-      @listener_exited = true
-      raise "No command was given" if command.empty?
-      kill_subprocess = proc do
-        begin
-          Process.kill("INT", @pid) if @pid
-        rescue Errno::ESRCH
-        end
-      end
-      trap("INT", &kill_subprocess)
-      at_exit(&kill_subprocess)
-      listener = Guard::Listener.select_and_init(:watchdir => Dir.pwd, :watch_all_modifications => true)
+      watch_dir = Dir.pwd
+      listener = Guard::Listener.select_and_init(:watchdir => watch_dir, :watch_all_modifications => true)
       listener.on_change {
-        @listener_exited = true
-        Process.kill("INT", @pid) if @pid
+        Thread.main.raise Reload
       }
-      listener_thread = Thread.new { listener.start }
-      listener_thread.abort_on_exception = true
-      while @listener_exited do
-        @listener_exited = false
-        @pid = fork do
-          exec(command.to_s)
+      Thread.new { listener.start }
+
+      at_exit { Process.kill("INT", @pid) rescue nil if @pid }
+
+      puts "[Starting ak47 #{VERSION} in #{watch_dir.inspect}]"
+      loop do
+        begin
+          puts "[Running... #{Time.new.to_s}]"
+          puts "# #{command}"
+          @pid = fork { exec(command) }
+          _, status = Process.waitpid2(@pid)
+          if status.success?
+            puts "[Terminated, waiting for file system change]"
+            sleep
+          else
+            puts "[Terminated abnormally (#{status.inspect}), retrying in 5s]"
+            sleep 5
+          end
+        rescue Reload
+          puts "[Reloading... #{Time.new.to_s}]"
         end
-        Process.waitpid(@pid)
       end
     end
   end
